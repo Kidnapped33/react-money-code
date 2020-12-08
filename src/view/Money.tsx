298fe5a68bf678d68c9d315885476494af454dc8
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-12-08 23:50:45 +0800

    完成 Money 的 支出收入 样式

--- src/view/Money.tsx
+++ src/view/Money.tsx
@@ -42,7 +42,27 @@ font-size: 14px;
   }
 `;
 const CategorySection = styled.section`
-border: 1px solid red;
+  font-size: 24px;
+    > ul {
+      display: flex;
+      background-color: #c4c4c4;
+      > li {
+        width: 50%;
+        text-align: center;
+        padding: 16px 0;
+        position: relative;
+          &.selected::after{
+           content: '';
+           display: block;
+           height: 3px;
+           background-color: #333;
+           position: absolute;
+           left: 0;
+           bottom: 0;
+           width: 100%;
+            }
+      }
+    }
 `;
 const NumberPadSection = styled.section``;
 
@@ -67,7 +87,7 @@ function Money() {
             </NotesSection>
             <CategorySection>
                 <ul>
-                    <li>支出</li>
+                    <li className={'selected'}>支出</li>
                     <li>收入</li>
                 </ul>
             </CategorySection>
