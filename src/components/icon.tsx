eb5a5253313edee4848016154f1837924431a57d
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-12-08 14:31:52 +0800

    封装 Icon 组件

---
+++ src/components/icon.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+require('icons/money.svg');
+require('icons/tag.svg');
+require('icons/chart.svg');
+
+//还可以优化为引入一个目录，而不需要引入单个svg
+
+type Props = {
+    name: string
+}
+
+const Icon = (props: Props) => {
+    return (
+        <svg className="icon">
+            <use xlinkHref={'#' + props.name}/>
+        </svg>
+    );
+};
+
+export default Icon;
