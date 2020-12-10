aac3a42ee7d73058ef42113aa82703d417e80b3a
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-12-10 15:12:40 +0800

    抽离 useTags.tsx 函数

--- src/view/Tags.tsx
+++ src/view/Tags.tsx
@@ -1,9 +1,17 @@
 import Layout from '../components/Layout';
-import React from 'react';
+import React, {useState} from 'react';
+import {useTags} from '../useTags';
 
 function Tags() {
+    const {tags, setTags} = useTags();
     return (
         <Layout>
+            <ol>
+                {tags.map(tag =>
+                    <li key={tag}>{tag}</li>
+                )}
+            </ol>
+
             <h2>标签页</h2>
         </Layout>
     );
--- src/view/money/TagsSection.tsx
+++ src/view/money/TagsSection.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import React, {useState} from 'react';
+import {useTags} from '../../useTags';
 
 const Wrapper = styled.section`
   flex-grow: 1;
@@ -36,7 +37,7 @@ type Props = {
     onChange: (selected: string[]) => void
 }
 const TagsSection: React.FC<Props> = (props) => {
-    const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+    const {tags, setTags} = useTags();
     const selectedTag = props.value;
     //const [selectedTag, setSelectedTag] = useState<string[]>([]);
     const onAddTag = () => {
