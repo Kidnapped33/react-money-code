1768fd1e50a56b9ce298554db2625dc30659a3f0
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-12-15 23:44:47 +0800

    hash 展示 统计页

--- src/view/Statistics.tsx
+++ src/view/Statistics.tsx
@@ -2,12 +2,17 @@ import Layout from '../components/Layout';
 import React, {useState} from 'react';
 import {CategorySection} from './money/CategorySection';
 import styled from 'styled-components';
-import {useRecords} from '../hooks/useRecords';
+import {RecordItem, useRecords} from '../hooks/useRecords';
 import {useTags} from '../hooks/useTags';
 import day from 'dayjs';
 
 const CategoryWrapper = styled.div`
  background-color: white;
+`;
+const Header = styled.h3`
+font-size: 18px;
+line-height: 20px;
+padding: 10px 16px;
 `;
 const Item = styled.div`
 display: flex;
@@ -28,26 +33,53 @@ function Statistics() {
     const [category, setCategory] = useState<'-' | '+'>('-');
     const {records} = useRecords();
     const {getName} = useTags();
+    const hash: { [K: string]: RecordItem[] } = {};
+    const selectedRecords = records.filter(r => r.category === category);
+    selectedRecords.map(r => {
+        const key = day(r.createTime).format('YYYY-MM-DD');
+        const value = r;
+        // key 不在 hash 里面，就先创建空数组，然后 push 进去
+        if (!(key in hash)) {
+            hash[key] = []; //变成数组才能push
+        }
+        hash[key].push(value);
+    });
+
+    const array = Object.entries(hash).sort((a, b) => {
+            if (a[0] === b[0]) return 0;
+            if (a[0] < b[0]) return -1;
+            if (a[0] > b[0]) return 1;
+            return 0;
+        }
+    );
+
+
     return (
         <Layout>
             <CategoryWrapper>
                 <CategorySection value={category}
                                  onChange={value => setCategory(value)}/>
             </CategoryWrapper>
-            <div>
-                {records.map(r => {
-                    {day(r.createTime).format('YYYY年MM月DD日');}
-                    return <Item>
-                        <div className="tags">
-                            {r.tagIds.map(tagId => <span>{getName(tagId)}</span>)}
-                        </div>
-                        {r.notes && <div className="note">
-                            {r.notes}
-                        </div>}
-                        ￥{r.amount}
-                    </Item>;
-                })}
-            </div>
+            {array.map(([date, records]) =>
+                <div>
+                    <Header>{date} 合计 {}</Header>
+
+                    <div>
+                        {records.map(r => {
+                            //key={r.createTime}
+                            return <Item>
+                                <div className="tags oneLine">
+                                    {r.tagIds.map(tagId => <span key={tagId}>{getName(tagId)}</span>)}
+                                </div>
+                                {r.notes && <div className="note">
+                                    {r.notes}
+                                </div>}
+                                ￥{r.amount}
+                            </Item>;
+                        })}
+                    </div>
+                </div>
+            )}
         </Layout>
     );
 }
