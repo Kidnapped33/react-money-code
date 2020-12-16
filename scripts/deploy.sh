69a3d38e1965c5f6743f8d96d610722843438b5d
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-12-16 17:36:00 +0800

    优å有å优 优化 deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-
+yarn build &&
+cd build/ &&
+git init &&
+git add . &&
+git commit -m"deploy" &&
+git remote add origin git@github.com:Kidnapped33/react-money-website.git &&
+git push -u origin master -f
+cd ../
